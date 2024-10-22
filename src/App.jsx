e8cabcd9c289d0f83af4a76b29e168daa789855c
Lenovo <ealenxie@qq.com>
2024-10-22 09:51:52 +0800

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <>
         <Music />
-        {/*<MusicPlay />*/}
+        {/* <MusicPlay /> */}
         {/* <h1 className="text-3xl font-bold underline">
           Hello world!
         </h1> */}
