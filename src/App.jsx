f3f5fd0d2276066d165ae354bc5191c29be8b2c3
Lenovo <ealenxie@qq.com>
2024-10-23 11:14:44 +0800

    主框架搭建

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 // 初始化css样式 ：yarn add normalize.css OR npm install normalize.css
 import 'normalize.css'
+import {LayoutIndex} from "@/components/Layout/LayoutIndex.jsx";
+import React from "react";
+import {useRoutes} from "react-router-dom";
 // 导入路由
 import routes from "@/router";
-import {useRoutes} from "react-router-dom";
 
 export default function App() {
     //根据路由表生成对应的路由规则
-    const element = useRoutes(routes)
+    const element = useRoutes(routes);
     return (
-        <div>{element}</div>
+        <>
+            <LayoutIndex element={element}/>
+        </>
     )
 }
---
+++ src/components/Layout/LayoutIndex.jsx
@@ -0,0 +1,121 @@
+// npm install antd --save
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    HeartOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+    MutedOutlined,
+    HomeOutlined
+} from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
+import React, {useState} from "react";
+import {useLocation, useNavigate} from "react-router-dom";
+const { Header, Sider, Content } = Layout;
+
+const items=[
+    {
+        label: '首页',
+        key: '/',
+        icon: <HomeOutlined />,
+    },
+    {
+        label: '发现音乐',
+        key: '/discover',
+        icon: <HeartOutlined />,
+    },
+    {
+        label: '歌手',
+        key: '/artist',
+        icon: <UserOutlined />,
+    },
+    {
+        label: '歌单',
+        key: '/playlist',
+        icon: <VideoCameraOutlined />,
+    },
+    {
+        label: '专辑',
+        key: '/album',
+        icon: <MutedOutlined />
+    }
+]
+export function LayoutIndex({element}) {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
+    const navigate = useNavigate()
+    const onMenuClick = ( value ) => {
+        navigate( value.key )
+    }
+
+    // 菜单高亮
+    const location = useLocation()
+    const selectedKey = location.pathname
+
+    return (
+        <>
+            {/*主框架*/}
+            <Layout style={{
+                minHeight: '100vh',
+            }}>
+                {/*侧边栏*/}
+                <Sider trigger={null} collapsible collapsed={collapsed} theme="light" style={{
+                    background: '#f1f5f9'
+                }}>
+                    <div className="demo-logo-vertical" ></div>
+                    {/*导航栏*/}
+                    <Menu
+                        theme="light"
+                        mode="inline"
+                        defaultSelectedKeys={[selectedKey]}
+                        selectedKeys={ selectedKey }
+                        items={items}
+                        onClick={ onMenuClick }
+                        style={{
+                            background: '#f1f5f9'
+                        }}
+                    />
+                </Sider>
+                <Layout>
+                    <Header
+                        style={{
+                            padding: 0,
+                            borderBottom: '1px solid rgb(235, 237, 240)',
+                            background: colorBgContainer,
+                        }}
+                    >
+                        <Button
+                            type="text"
+                            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                            onClick={() => setCollapsed(!collapsed)}
+                            style={{
+                                fontSize: '16px',
+                                width: 64,
+                                height: 64,
+                            }}
+                        />
+                    </Header>
+                    {/*路由页面*/}
+                    <Content
+                        style={{
+                            // margin: '24px 16px',
+                            padding: 24,
+                            minHeight: 280,
+                            background: colorBgContainer,
+                            borderRadius: borderRadiusLG,
+                        }}
+                    >
+                        {/* 指定路由组件呈现的位置 */}
+                        <div>{element}</div>
+                    </Content>
+                </Layout>
+            </Layout>
+            {/*播放列表*/}
+            {/*全局播放器*/}
+            {/*全屏播放器*/}
+        </>
+    )
+}
