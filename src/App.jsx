5e84a3554400770e693feedf85adac04e0e4d460
Lenovo <ealenxie@qq.com>
2024-10-30 14:25:02 +0800

    首页播放器

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 // 初始化css样式 ：yarn add normalize.css OR npm install normalize.css
-import 'normalize.css'
+// import 'normalize.css'
 import {LayoutIndex} from "@/components/Layout/LayoutIndex.jsx";
 import React from "react";
 import {useRoutes} from "react-router-dom";
--- src/components/Player/MainPlayer.jsx
+++ src/components/Player/MainPlayer.jsx
@@ -1,20 +1,55 @@
-import {Slider,Affix} from "antd";
+import {Slider,Affix,Button,Divider, Space, Typography} from "antd";
+import { PlayCircleOutlined, StepBackwardOutlined, StepForwardOutlined, RetweetOutlined, UnorderedListOutlined,MenuUnfoldOutlined,SoundOutlined } from '@ant-design/icons';
 import './MainPlayer.scss'
+import coverImg from '@/assets/placeholder.svg'
 export default function MainPlayer(){
     return (
         <div className="main-player">
             {/* 播放信息 */}
             <div className="play-data">
-                111
+                <div className="cover">
+                    <img src={coverImg} class="cover-img" />
+                </div>
+                <div className="info">
+                    <h3 className="name">歌曲名称</h3>
+                    <span className="artists">歌手</span>
+                </div>
             </div>
             {/* 控制 */}
             <div className="play-control">
-                <div>222</div>
-                <Slider defaultValue={30} className="player-slider"/>
+                <div className="play-icon">
+                    <Button shape="circle" icon={<StepBackwardOutlined />} />
+                    <Button shape="circle" icon={<PlayCircleOutlined style={{fontSize:"30px"}}/>} size="large" />
+                    <Button shape="circle" icon={<StepForwardOutlined />} />
+                </div>
+                <div className="player-slider">
+                    <span className="player-time">00:00</span>
+                    <Slider
+                        min={0}
+                        max={274}
+                        step={0.01}
+                        defaultValue={0}
+                        className="player-seekbar"
+                        />  
+                    <span className="player-time">04:34</span>
+                </div>
+                
             </div>
             {/* 功能 */}
             <div className="play-menu">
-                333
+                <div className="volume-col">
+                    <SoundOutlined style={{fontSize:"20px",cursor:"pointer"}}/>
+                    <Slider
+                            min={0}
+                            max={100}
+                            step={1}
+                            defaultValue={0}
+                            className="player-volume"
+                            />  
+                    <span className="vertical"></span>
+                </div>
+                
+                <MenuUnfoldOutlined style={{fontSize:"20px",cursor:"pointer"}}/>
             </div>
         </div>
     )
